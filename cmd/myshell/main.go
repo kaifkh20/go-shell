c092fa2764260769ea4820354048dc12bdb93dee
kaifkh20 <kaifkhan.biz@gmail.com>
2024-05-28 21:44:20 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -12,11 +12,12 @@ func main() {
 	// fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+  for ; ;{
+  fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
   cmnd,_:=bufio.NewReader(os.Stdin).ReadString('\n')
   cmnd = strings.TrimSpace(cmnd)
   fmt.Fprintf(os.Stdout,"%s: command not found\n",cmnd)
-
+  }
 }
